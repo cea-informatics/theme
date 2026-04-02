73186149995fd1965afa4cd6b43e7c16d4f7b8c3
dwesh163 <dwesh163@protonmail.com>
2026-04-02 09:50:33 +0200

    [refactor] remove unused script enqueueing in functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,5 @@
 <?php
 
-add_action('wp_enqueue_scripts', function () {
-	wp_enqueue_style(
-		'foxtown-parent-style',
-		get_template_directory_uri() . '/style.css'
-	);
-});
-
 add_action('wp_head', function () { ?>
 	<style>
 		:root { --theme-palette-color-1: #FF6600; }
