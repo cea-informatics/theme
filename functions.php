58e90e59d66f8c65154aa4b46f459b1dfcb58b2c
dwesh163 <dwesh163@protonmail.com>
2026-04-02 07:25:31 +0200

    [refactor] update theme palette color in functions.php

--- functions.php
+++ functions.php
@@ -9,6 +9,6 @@ add_action('wp_enqueue_scripts', function () {
 
 add_action('wp_head', function () { ?>
 	<style>
-		:root { --theme-palette-color-1: #0055A4; }
+		:root { --theme-palette-color-1: #FF6600; }
 	</style>
 <?php }, 100);
